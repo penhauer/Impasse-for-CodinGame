50d06cd6fdc87105af874cc0aa8c5705537ab3c9
Balazs G. Horvath <bghorvath@pm.me>
2022-09-12 15:02:47 +0200

    add basic functions for classes

--- game/board.cpp
+++ game/board.cpp
@@ -1,17 +1,48 @@
 #include "common.h"
 
-void Board() {
-    // init board with grid for the Pieces
-    for(int i = 0; i < 8; i++)
+class Board
     {
-        for(int j = 0; j < 8; j++)
-        {
-            if(i < 3 && (i+j)%2 == 1)
-                board[i][j] = new int Piece(1, i, j);
-            else if(i > 4 && (i+j)%2 == 1)
-                board[i][j] = new int Piece(2, i, j);
-            else
-                board[i][j] = 0;
-        }
+        private:
+            int turn; // 1 = white, -1 = black
+            int gameState; // 0 = in progress, 1 = white win, -1 = black win
+            int whiteSingles; //number of white singles
+            int whiteDoubles; //number of white doubles
+            int blackSingles; //number of black singles
+            int blackDoubles; //number of black doubles
     };
-}
+    {
+        public:
+            Board
+            {
+                turn = 1;
+                gameState = 0;
+                whiteSingles = 6;
+                whiteDoubles = 6;
+                blackSingles = 6;
+                blackDoubles = 6;
+            }
+            void selectPiece(int x, int y) {
+                selectedPiece = board[x][y];
+                if (turn == selectedPiece.color) {
+                    bool pieceSelected = true;
+                }
+            }
+            void movePiece(int x, int y, int newX, int newY) {
+                board[newX][newY] = board[x][y];
+                board[x][y] = 0;
+                turn = turn * -1;
+            }
+
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    if(i < 3 && (i+j)%2 == 1)
+                        board[i][j] = new int Piece(1, i, j);
+                    else if(i > 4 && (i+j)%2 == 1)
+                        board[i][j] = new int Piece(2, i, j);
+                    else
+                        board[i][j] = 0;
+                }
+            }
+    };
--- game/piece.cpp
+++ game/piece.cpp
@@ -1,7 +1,42 @@
 #include "common.h"
 
-void Piece(int color, int x, int y) {
-    color = color;
-    x = x;
-    y = y;
-}
+class Piece
+{
+    private:
+        int color; // 1 = white, -1 = black
+        int x; //x coordinate
+        int y; //y coordinate
+        bool isDouble; // true = double, false = single
+};
+{
+    public:
+        Piece(int color, int x, int y, bool isDouble)
+        {
+            this->color = color;
+            this->x = x;
+            this->y = y;
+            this->isDouble = isDouble;
+        }
+        void changeType(bool isDouble)
+        {
+            this->isDouble = isDouble;
+        }
+        void move(int newX, int newY)
+        {
+            this->x = newX;
+            this->y = newY;
+        }
+        bool getType()
+        {
+            return isDouble
+        }
+        int getColor()
+        {
+            return color
+        }
+        void getMoves(int board[8][8])
+        {
+            //return array of possible moves
+
+        }
+};
