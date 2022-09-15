dd5778fb79f1b74f717ab86b014895d915b33f49
Balazs G. Horvath <bghorvath@pm.me>
2022-09-15 14:44:00 +0200

    add create_board method and move method boilerplate

--- game/board.cpp
+++ game/board.cpp
@@ -1,48 +1,91 @@
-#include "common.h"
+#include "common.h";
 
 class Board
+{
+private:
+    int turn; // 1 = white, -1 = black
+    int gameState; // 0 = in progress, 1 = white win, -1 = black win
+    int whiteSingles; //number of white singles
+    int whiteDoubles; //number of white doubles
+    int blackSingles; //number of black singles
+    int blackDoubles; //number of black doubles
+    int board[8][8];
+public:
+    Board
     {
-        private:
-            int turn; // 1 = white, -1 = black
-            int gameState; // 0 = in progress, 1 = white win, -1 = black win
-            int whiteSingles; //number of white singles
-            int whiteDoubles; //number of white doubles
-            int blackSingles; //number of black singles
-            int blackDoubles; //number of black doubles
-    };
-    {
-        public:
-            Board
-            {
-                turn = 1;
-                gameState = 0;
-                whiteSingles = 6;
-                whiteDoubles = 6;
-                blackSingles = 6;
-                blackDoubles = 6;
-            }
-            void selectPiece(int x, int y) {
-                selectedPiece = board[x][y];
-                if (turn == selectedPiece.color) {
-                    bool pieceSelected = true;
+        turn = 1;
+        gameState = 0;
+        whiteSingles = 6;
+        whiteDoubles = 6;
+        blackSingles = 6;
+        blackDoubles = 6;
+        board = create_board();
+    }
+    int[8][8] create_board() {
+        board = new int[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                // 1: (0,0), (1,3), (0,4), (1,7)
+                // 2: (7,1), (6,2), (7,5), (6,6)
+                // -1: (6,0), (7,3), (6,4), (7,7)
+                // -2: (1,1), (0,2), (1,5), (0,6)
+                if (j < 2) and ((i+j) %% 4) {
+                    board[i][j] = 1
+                } else if (j > 5) and ((i+j) %% 4 == 0) {
+                    board[i][j] = 2
+                } else  if (j > 5) and ((i+j) %% 4 == 2) {
+                    board[i][j] = -1;
+                } else if (j < 2) and ((i+j) %% 4 == 2) {
+                    board[i][j] = -2;
+                } else {
+                    board[i][j] = 0;
                 }
             }
-            void movePiece(int x, int y, int newX, int newY) {
-                board[newX][newY] = board[x][y];
-                board[x][y] = 0;
-                turn = turn * -1;
-            }
+        }
+        return board;
+    }
+    void getMoves(int x, int y) {
+        // return array of possible moves
+        Piece piece = board[x][y];
 
-            for(int i = 0; i < 8; i++)
-            {
-                for(int j = 0; j < 8; j++)
-                {
-                    if(i < 3 && (i+j)%2 == 1)
-                        board[i][j] = new int Piece(1, i, j);
-                    else if(i > 4 && (i+j)%2 == 1)
-                        board[i][j] = new int Piece(2, i, j);
-                    else
-                        board[i][j] = 0;
+    };
+    void checkDiagonals(int x, int y, bool forward) {
+        // check diagonals forward
+        while ((int j > 0) & (int j < 8)) {
+            int j = y + 1;
+            if (forward) {
+                for (int i = x + 1; i < 8; i++) {
+                    if (board[i][j] == 0) {
+                        // add to possible moves
+                    } else {
+                        break;
+                    }
                 }
-            }
+                // code here
+                j = j + 1;
+            } else {
+                // code here
+                j = j - 1;
+            };
+        };
+    };
+
+    void movePiece(int x, int y, int newX, int newY) {
+        board[newX][newY] = board[x][y];
+        board[x][y] = 0;
+        turn = turn * -1;
+    }
+
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            if(i < 3 && (i+j)%2 == 1)
+                board[i][j] = new int Piece(1, i, j);
+            else if(i > 4 && (i+j)%2 == 1)
+                board[i][j] = new int Piece(2, i, j);
+            else
+                board[i][j] = 0;
+        }
+    }
     };
--- game/piece.cpp
+++ game/piece.cpp
@@ -2,41 +2,55 @@
 
 class Piece
 {
-    private:
-        int color; // 1 = white, -1 = black
-        int x; //x coordinate
-        int y; //y coordinate
-        bool isDouble; // true = double, false = single
-};
-{
-    public:
-        Piece(int color, int x, int y, bool isDouble)
-        {
-            this->color = color;
-            this->x = x;
-            this->y = y;
-            this->isDouble = isDouble;
-        }
-        void changeType(bool isDouble)
-        {
-            this->isDouble = isDouble;
-        }
-        void move(int newX, int newY)
-        {
-            this->x = newX;
-            this->y = newY;
-        }
-        bool getType()
-        {
-            return isDouble
-        }
-        int getColor()
-        {
-            return color
-        }
-        void getMoves(int board[8][8])
-        {
-            //return array of possible moves
+private:
+    int color; // 1 = white, -1 = black
+    int x;
+    int y;
+    bool isDouble; // true = double, false = single
+public:
+    // constructor
+    Piece(int color, int x, int y, bool isDouble)
+    {
+        this->color = color;
+        this->x = x;
+        this->y = y;
+        this->isDouble = isDouble;
+    }
 
-        }
+    // getters
+    bool getType()
+    {
+        return isDouble
+    }
+    int getColor()
+    {
+        return color
+    }
+    // setters
+    void setDouble(bool isDouble)
+    {
+        this->isDouble = isDouble;
+    }
+    // move piece
+    void move(int newX, int newY)
+    {
+        this->x = newX;
+        this->y = newY;
+    }
+    //move piece by offset
+    void moveBy(int offsetX, int offsetY)
+    {
+        this->x += offsetX;
+        this->y += offsetY;
+    }
+
+
+    // get possible moves for the piece
+    void getMoves(int board[8][8])
+    {
+        //return array of possible moves
+
+    }
 };
+
+piece = new Piece(1, 0, 0, false);
