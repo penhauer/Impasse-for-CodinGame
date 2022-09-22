f41b7b96043c0564e8b166f750ecabb080faaeca
Balazs G. Horvath <bghorvath@pm.me>
2022-09-22 15:39:29 +0200

    gamestate restructuring for easy readability

--- src/gamestate.cpp
+++ src/gamestate.cpp
@@ -1,36 +1,27 @@
-#include <unordered_map>
+#include <map>
 #include <unordered_set>
 #include <tuple>
 
-struct Piece
-{
-    int color;     // 1 = white, -1 = black
-    bool isDouble; // true = double, false = single
-    Piece (int color, bool isDouble) : color(color), isDouble(isDouble) {}
-};
-
 // Simple struct to represent a move
 struct Move {
-    int from;
-    int to;
+    int piece;
+    std::pair<int,int> from;
+    std::pair<int,int> to;
 };
 
-// Map of pieces on the board
-typedef std::unordered_map<int, Piece> PieceMap;
-// Map of all possible moves at the moment
-typedef std::unordered_map<int, std::unordered_set<int>> MoveMap;
-
 // Map of the board represented by integers
-typedef std::unordered_map<int, std::unordered_map<int, int>> Board;
+typedef std::map<int, std::map<int, int>> Board;
 
-int getPosition(int col, int row)
-{
-    return col + row * 8;
-}
+// Map of all possible moves at the moment
+typedef std::map<std::pair<int,int>, std::unordered_set<int>> MoveMap;
 
-std::tuple<int, int> getColRow(int position) {
-    return {position % 8, position / 8};
-}
+struct Pieces {
+    int whiteSingles; // number of white singles
+    int whiteDoubles; // number of white doubles
+    int blackSingles; // number of black singles
+    int blackDoubles; // number of black doubles
+    Pieces () : whiteSingles(6), whiteDoubles(6), blackSingles(6), blackDoubles(6) {};
+};
 
 class GameState
 {
@@ -38,48 +29,37 @@ public:
     int player; // 0 = Ai vs Ai, 1 = White vs Ai, -1 = Black vs Ai, 2 = White vs Black
     int turn;         // 1 = white, -1 = black
     int state;    // 0 = in progress, 1 = white win, -1 = black win
-    int whiteSingles; // number of white singles
-    int whiteDoubles; // number of white doubles
-    int blackSingles; // number of black singles
-    int blackDoubles; // number of black doubles
+    Pieces pieces;
     Board board;
-    PieceMap piecemap;
     GameState()
     {
         player = 1;
         turn = 1;
         state = 0;
-        whiteSingles = 6;
-        whiteDoubles = 6;
-        blackSingles = 6;
-        blackDoubles = 6;
+        pieces = Pieces();
         for (int col = 0; col < 8; col++)
         {
             for (int row = 0; row < 8; row++)
             {
-                // 1: (0,0), (3,1), (4,0), (1,7)
-                // 2: (1,7), (2,6), (5,7), (6,6)
-                // -1: (0,6), (3,7), (4,6), (7,7)
-                // -2: (1,1), (2,0), (5,1), (6,0)
+                // white single (1): (0,0), (3,1), (4,0), (1,7)
+                // white double (2): (1,7), (2,6), (5,7), (6,6)
+                // black single (-1): (0,6), (3,7), (4,6), (7,7)
+                // black double (-2): (1,1), (2,0), (5,1), (6,0)
                 if (row < 2 && (col + row) % 4 == 0)
                 {
                     board[col][row] = 1;
-                    piecemap[getPosition(col, row)] = Piece(1, false);
                 }
                 else if (row > 5 && (col + row) % 4 == 0)
                 {
                     board[col][row] = 2;
-                    piecemap[getPosition(col, row)] = Piece(1, true);
                 }
                 else if ((row > 5) && ((col + row) % 4 == 2))
                 {
                     board[col][row] = -1;
-                    piecemap[getPosition(col, row)] = Piece(-1, false);
                 }
                 else if (row < 2 && (col + row) % 4 == 2)
                 {
                     board[col][row] = -2;
-                    piecemap[getPosition(col, row)] = Piece(-1, true);
                 }
                 else
                 {
@@ -88,53 +68,28 @@ public:
             };
         };
     };
-    int getDirection(int color, int isDouble) const
+    int pieceDirection(int piece) const
     {
-        if ((color == 1 && !isDouble) | (color == -1 && isDouble))
-        {
-            return 1;
-        }
-        else
-        {
-            return -1;
-        };
+        return (piece == 1 || piece == -2) ? 1 : -1;
     };
-    void movePiece(Move move)
+    void movePiece(const Move& move)
     {
-        int col, row, newCol, newRow;
-        std::tie(col, row) = getColRow(move.from);
-        std::tie(newCol, newRow) = getColRow(move.to);
-        board[newCol][newRow] = board[col][row];
-        board[col][row] = 0;
-        piecemap[move.to] = piecemap[move.from];
-        piecemap.erase(getPosition(col, row));
+        board[move.to.first][move.to.second] = board[move.from.first][move.from.second];
+        board[move.from.first][move.from.second] = 0;
     };
-    void removePiece(int col, int row)
-    {
-        board[col][row] = 0;
-        piecemap.erase(getPosition(col, row));
+    void changePieceType(int col, int row) {
+        board[col][row] = board[col][row] % 2 == 0 ? board[col][row] / 2 : board[col][row] * 2;
     };
-    void changePieceType(int col, int row)
-    {
-        if (piecemap[getPosition(col, row)].isDouble == true)
-        {
-            piecemap[getPosition(col, row)].isDouble = false;
-        }
-        else
-        {
-            piecemap[getPosition(col, row)].isDouble = true;
-        };
-    };
-    MoveMap checkDiagonals(int col, int row, bool forward) const
+    MoveMap checkDiagonals(int col, int row) const
     {
         // check diagonals forward
         MoveMap movemap;
-        while (row > 0 && row < 8)
+        while (row < 8)
         {
-            if (forward)
-            {
-                for (int col = col + 1; col < 8; col++)
+                for (col < 8; col++)
                 {
+                    int piece = board[col][row];
+                    if (pieceDirection() == 1) {
                     if (board.at(col).at(row) == 0)
                     {
                         movemap[getPosition(col, row)].insert(col, row);
@@ -179,7 +134,7 @@ public:
                 Piece piece = piecemap[getPosition(col, row)];
                 if (piece.color == color)
                 {
-                    int direction = getDirection(piece.color, piece.isDouble);
+                    int direction = pieceDirection(piece.color, piece.isDouble);
                     MoveMap new_moves = checkDiagonals(col, row, direction);
                     moves.insert(new_moves.begin(), new_moves.end());
                 };
